ef7bc8178bea549ed0880049bde78c078936994a
franksun <franksun913@gmail.com>
2024-11-05 15:29:55 -0600

    Added Main_Page and an empty HomePageActivity

--- app/src/main/java/com/cs407/flow/HomePageActivity.kt
+++ app/src/main/java/com/cs407/flow/HomePageActivity.kt
@@ -1,27 +1,17 @@
 package com.cs407.flow
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val startButton: Button = findViewById(R.id.start_button)
-        startButton.setOnClickListener{
-            val intent = Intent(this@MainActivity, HomePageActivity::class.java)
-            startActivity(intent)
-        }
-
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_page)) { v, insets ->
+        setContentView(R.layout.activity_home_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
